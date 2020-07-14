de7f2c23c0324c1aedcc384cd350705f0da7f0f7
Casey <innerforest7@gmail.com>
2020-07-14 16:12:48 -0700

    switch / route back to landingPage

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended : false }));
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 app.get('/', asyncHandler(async (req, res) => {
-  res.send('HOME');
+  res.render('landing-Page');
 }));
 
 app.get('/login-page', asyncHandler(async (req, res) => {

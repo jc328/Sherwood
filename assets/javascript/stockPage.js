73b7f61703ab0afde51918c72b531ed226b54a90
John Chen <johnchen328@gmail.com>
2020-07-19 20:51:18 -0700

    changed buy forms

--- assets/javascript/stockPage.js
+++ assets/javascript/stockPage.js
@@ -6,14 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let marketPrice = document.querySelector('.marketPrice')
     let marketPriceValue = document.createElement('h4')
     marketPriceValue.innerHTML = currentPrice
-    // title.innerHTML += ` - $${currentPrice} | Sherwood`
     marketPrice.appendChild(marketPriceValue)
 
     document.getElementById("number-shares__buy")
         .addEventListener('change', e => {
             let estimateCost = (e.target.value * currentPrice).toFixed(2);
             console.log(e.target.value)
-            costSpan.innerHTML = `$${estimateCost}`;
+            costSpan.innerHTML = `   $${estimateCost}`;
         });
 
     document.getElementById("number-shares__sell")
@@ -22,19 +21,25 @@ document.addEventListener("DOMContentLoaded", () => {
             creditSpan.innerHTML = `$${estimateCredit}`;
         });
 
+    let buyButton = document.getElementById("buy-button")
+
     let buyTabButton = document.getElementById("buy-tab__button");
     let sellTabButton = document.getElementById("sell-tab__button");
     let buyForm = document.getElementById("transaction-form__buy");
     let sellForm = document.getElementById("transaction-form__sell");
 
     sellTabButton.addEventListener('click', e => {
-        if(!buyForm.classList.contains("hidden") && sellForm.classList.contains("hidden")) {
-            buyForm.classList.add("hidden");
-            sellForm.classList.remove("hidden");
-        }
+        // if(!buyForm.classList.contains("hidden") && sellForm.classList.contains("hidden")) {
+        //     buyForm.classList.add("hidden");
+        //     sellForm.classList.remove("hidden");
+        // }
+        buyButton.innerHTML = 'Submit Sell'
+        buyButton.style.background = 'red'
     })
 
     buyTabButton.addEventListener("click", e => {
+        buyButton.innerHTML = 'Submit Buy'
+        buyButton.style.background = '#77d42a'
         if(!sellForm.classList.contains("hidden") && buyForm.classList.contains("hidden")) {
             sellForm.classList.add("hidden");
             buyForm.classList.remove("hidden");
